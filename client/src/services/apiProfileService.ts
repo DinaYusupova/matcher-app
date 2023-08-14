491d1b25f1aeaa54ba654ec4d8ae4193f5d665c1
DinaYusupova <dina5955@gmail.com>
2023-08-14 14:09:55 +0300

    geo service'

--- client/src/services/apiProfileService.ts
+++ client/src/services/apiProfileService.ts
@@ -14,12 +14,12 @@ export const fetchProfileService = async (): Promise<ProfileModelType[]> => {
 
 export const likeProfileService: ApiServiceType = async (userId) => {
   const { data } = await apiService.post<ProfileModelType[]>('/profile/like', { userId });
-  console.log(data[0].distanceBetweenUsers, "ДИСТАНЦИЯ", data[0].userLatitude, "LATITUDE", data[0].userLongitude, "Longitude")
+  console.log(data[0].name, data[0].distanceBetweenUsers, "ДИСТАНЦИЯ", data[0].userLatitude, "LATITUDE", data[0].userLongitude, "Longitude")
   return { data, userId };
 };
 
 export const dislikeProfileService: ApiServiceType = async (userId) => {
   const { data } = await apiService<ProfileModelType[]>(`/profile/dislike/${userId}`);
-  console.log(data[0].distanceBetweenUsers, "ДИСТАНЦИЯ", data[0].userLatitude, "LATITUDE", data[0].userLongitude, "Longitude")
+  console.log(data[0].name, data[0].distanceBetweenUsers, "ДИСТАНЦИЯ", data[0].userLatitude, "LATITUDE", data[0].userLongitude, "Longitude")
   return { data, userId };
 };
